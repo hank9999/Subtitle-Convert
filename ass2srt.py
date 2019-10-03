c65b3ea898696df2a4bcede08446a0e903cb73b9
hank9999 <1825355690@qq.com>
2019-10-03 21:48:30 +0800

    Update ass2srt.py

--- ass2srt.py
+++ ass2srt.py
@@ -1,11 +1,12 @@
 try:
+	print("Convert Started\n")
 	count = 0
-	with open('2.ass','r',encoding='utf-8') as f1,open('2.ass.srt', 'w', encoding='utf-8') as f2:
+	with open('2.ass','r',encoding='utf-8') as f1,open('2.ass.srt', 'w', encoding='utf-8') as f2: //change input and output files
 		for line in f1.readlines():
 			count = count + 1
 			a = line.strip()
 			f2.write(str(count) + "\n" + a[a.find(",") + 1:a.find(",") + 11] + " --> " + a[a.find(",") + 12:a.find(",") + 22] + "\n" + a[a.find("}") + 1:] + "\n\n")
 	count = 0
-	print("")
+	print("Convert Completed\n")
 except:
-	print("")
+	print("Convert Failed")
