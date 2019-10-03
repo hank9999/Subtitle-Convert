6a8aa9e7b0037867e08de83149410e10d34ccc84
hank9999 <1825355690@qq.com>
2019-10-03 21:53:48 +0800

    Update ass2lrc.py

--- ass2lrc.py
+++ ass2lrc.py
@@ -1,6 +1,6 @@
 try:
   print("Convert Started\n")
-	with open('1.ass','r',encoding='utf-8') as f1,open('1.ass.lrc', 'w', encoding='utf-8') as f2: //change input and output files
+	with open('1.ass', 'r', encoding='utf-8') as f1, open('1.ass.lrc', 'w', encoding='utf-8') as f2: #change input and output files
 		for line in f1.readlines():
 			a = line.strip()
 			f2.write("[" + a[a.find(",") + 3:a.find(",") + 11] + "]" + a[a.find("}") + 1:] + "\n")
